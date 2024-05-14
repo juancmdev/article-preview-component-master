bcfa39d0843c6814d8df4c2660710e374e2fb6bd
juancmdev <juancmorenoj@gmail.com>
2024-05-13 22:58:11 -0500

    creating share-mobile active class

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const btn = document.querySelector("#btn");
 const share = document.querySelector("#share");
 
+if (window.matchMedia("(max-width: 768px)").matches) {
+  console.log("/* La pantalla tiene al menos 768 píxeles de ancho */");
+} else {
+  console.log("/* La pantalla no tiene al menos 400 píxeles de ancho */");
+}
+
 btn.addEventListener("click", () => {
   share.classList.toggle("share");
 });
