d0ea232b59252b95c2356f091c6e37e2cfd92450
juancmdev <juancmorenoj@gmail.com>
2024-06-03 14:28:19 -0500

    Index class added

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const btn = document.querySelector("#btn");
 const btn_mobile = document.querySelector(".btn-mobile");
 const share = document.querySelector("#share");
 const share_mobile = document.querySelector("#share-mobile");
+const profile = document.querySelector(".profile-information");
 
 if (window.matchMedia("(max-width: 768px)").matches) {
   btn_mobile.addEventListener("click", () => {
-    share_mobile.classList.toggle("display");
+    profile.classList.remove("index");
+    btn.classList.remove("index");
+    share_mobile.classList.add("index");
   });
 }
 
