a44244aae2604ac748a15b150064626a8f809320
juancmdev <juancmorenoj@gmail.com>
2024-06-03 19:52:48 -0500

    z-index class added to mobile design

--- app.js
+++ app.js
@@ -4,14 +4,36 @@ const share = document.querySelector("#share");
 const share_mobile = document.querySelector("#share-mobile");
 const profile = document.querySelector(".profile-information");
 
-if (window.matchMedia("(max-width: 768px)").matches) {
-  btn_mobile.addEventListener("click", () => {
+const mediaQuery = window.matchMedia("(max-width: 768px)");
+
+mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+function handleMediaQueryChange(e) {
+  if (e.matches) {
+    share_mobile.classList.add("index");
+    btn.addEventListener("click", () => {
+      profile.classList.add("index");
+      btn.classList.add("index");
+      share_mobile.classList.remove("index");
+    });
+  } else {
+    share_mobile.classList.remove("index");
     profile.classList.remove("index");
     btn.classList.remove("index");
-    share_mobile.classList.add("index");
-  });
+
+    btn.addEventListener("click", () => {
+      console.log("Detectando");
+      profile.classList.remove("index");
+      btn.classList.remove("index");
+      share.classList.toggle("share");
+    });
+  }
 }
 
-btn.addEventListener("click", () => {
-  share.classList.toggle("share");
+handleMediaQueryChange(mediaQuery);
+
+btn_mobile.addEventListener("click", () => {
+  share_mobile.classList.add("index");
+  profile.classList.remove("index");
+  btn.classList.remove("index");
 });
